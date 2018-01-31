297e9bdfc7f7e02afeb588f3389623f8bb899f16
1weidong <649149488@qq.com>
2018-01-31 13:45:24 +0800

    shopping

---
+++ main.js
@@ -0,0 +1,88 @@
+//main.js
+//导入数据
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Routers from './router.js';
+import Vuex from 'vuex';
+import App from './app.vue';
+import './style.css';
+
+import product_data from './product.js';
+
+Vue.use(VueRouter);
+Vue.use(Vuex);
+
+//路由配置
+const RouterConfig = {
+    //使用html5的History模式
+    mode: 'history',
+    routes: Routers
+};
+const router = new VueRouter(RouterConfig);
+
+router.beforeEach((to, from, next) => {
+    window.document.title = to.meta.title;
+    next();
+});
+
+router.afterEach((to, from, next) => {
+    window.scrollTo(0, 0);
+});
+
+//数据重组
+function getFilterArray (array) {
+    const res = [];
+    const json = {};
+    for (let i = 0; i < array.length; i++) {
+        const _self = array[i];
+        if (!json[_self]) {
+            res.push(_self);
+            json[_self] = 1;
+        }
+    }
+    return res;
+}
+
+
+const store = new Vuex.Store({
+   state: {
+        //商品列表数据
+       productList: [],
+       //购物车数据
+       cartList: []
+   },
+    getters: {
+        brands: state => {
+            const brands = state.productList.map(item => item.brand);
+            return getFilterArray(brands);
+        },
+        colors: state => {
+            const colors = state.productList.map(item => item.color);
+            return getFilterArray(colors);
+        }
+    },
+   mutations: {
+        //添加商品列表
+       setProductList (state, data) {
+           state.productList = data;
+       }
+   },
+   actions: {
+        //请求商品列表
+       getProductList (context) {
+           //真实环境下通过Ajax获取，这里用异步模拟
+           setTimeout ( () => {
+              context.commit('setProductList', product_data);
+           }, 500);
+       }
+   }
+});
+
+new Vue({
+   el: "#app",
+   router: router,
+   store: store,
+   render: h => {
+       return h(App);
+   }
+});
---
+++ router.js
@@ -0,0 +1,23 @@
+//router.js
+const routers = [
+    {
+        path: '/list',
+        meta: {
+            title: '商品列表'
+        },
+        component: (resolve) => require(['./views/list.vue'], resolve)
+    },
+    {
+        path: '/product/:id',
+        meta: {
+            title: '商品详情'
+        },
+        component: (resolve) => require(['./views/product.vue'], resolve)
+    },
+    {
+        path: '*',
+        redirecet: '/list'
+    }
+];
+
+export default routers;
