2e5ba633b33c5bea9ffccf6ed0c6402fa93a2669
1weidong <649149488@qq.com>
2018-02-01 17:15:49 +0800

    cart complete

--- main.js
+++ main.js
@@ -62,9 +62,36 @@ const store = new Vuex.Store({
         }
     },
    mutations: {
-        //添加商品列表
+       //清空购物车
+       emptyCart (state) {
+         state.cartList = [];
+       },
+       //修改商品数量
+       editCartCount (state, payload) {
+          const product = state.cartList.find(item => item.id === payload.id);
+          product.count += payload.count;
+       },
+       //删除商品
+       deleteCart (state, id) {
+          const index = state.cartList.findIndex(item => item.id === id);
+          state.cartList.splice(index, 1);
+       },
+       //添加商品列表
        setProductList (state, data) {
            state.productList = data;
+       },
+       //添加购物车
+       addCart(state, id) {
+           //先判断购物车是否已有，如果有，数量+1
+           const isAdded = state.cartList.find(item => item.id === id);
+           if (isAdded) {
+               isAdded.count ++;
+           } else {
+               state.cartList.push({
+                   id: id,
+                   count: 1
+               })
+           }
        }
    },
    actions: {
@@ -74,6 +101,16 @@ const store = new Vuex.Store({
            setTimeout ( () => {
               context.commit('setProductList', product_data);
            }, 500);
+       },
+       //购买
+       buy (context) {
+           //真实环境应通过Ajax提交购买请求后再清空购物列表
+           return new Promise(resolve=> {
+               setTimeout(()=> {
+                  context.commit('emptyCart');
+                  resolve();
+               }, 500);
+           })
        }
    }
 });
